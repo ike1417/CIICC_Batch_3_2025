728ec7d270007e9d0f1b98c96d745f50ddd27562
ike1417 <istudenthernan14@gmail.com>
2025-07-21 02:05:11 +0000

    Done task3 in chapter 2

---
+++ Chapter2/Task3.java
@@ -0,0 +1,16 @@
+public class Task3 {
+    public static void main(String[] args) {
+        String a = "Wow";          
+        String b = "Wow";           
+        String c = new String(""); 
+        String d = "Wow!";          
+
+        boolean bl = a == b;                     
+        boolean b2 = d.equals(b + "!");          
+        boolean b3 = !c.equals(a);              
+
+        if (bl && b2 && b3) {
+            System.out.println("Success!");
+        }
+    }
+}
