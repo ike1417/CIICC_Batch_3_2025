d112b5b365f4112083e73a93174546fa1e45a960
ike1417 <istudenthernan14@gmail.com>
2025-07-21 01:51:59 +0000

    Done chapter task2

---
+++ Chapter2/Task2.java
@@ -0,0 +1,17 @@
+public class Task2 {
+    public static void main(String[] args) {
+        
+        byte b = 3;
+        short s = 1;
+        int i = 1;
+        float f = 2.0f;
+        char c1 = 'H';
+        char c2 = '0';
+        boolean bol = true;
+
+        
+        String result = "" + c1 + b + s + i + c2 + " w0rld " + f + " " + bol;
+
+        System.out.println(result);
+    }
+}
