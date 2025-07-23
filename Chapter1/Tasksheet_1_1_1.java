30025e441fc7387533d30da6a58867b92f96f841
ike1417 <istudenthernan14@gmail.com>
2025-07-23 05:26:25 +0000

    Done tasksheet-1-1-1 in chapter1

---
+++ Chapter1/Tasksheet_1_1_1.java
@@ -0,0 +1,14 @@
+package Chapter1;
+
+public class Tasksheet_1_1_1 {
+    /** 
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+
+    public static void main(String[] args) {
+        System.out.println("Kamusta Mundo!"); // Display the string.
+    }
+}
+    
+
