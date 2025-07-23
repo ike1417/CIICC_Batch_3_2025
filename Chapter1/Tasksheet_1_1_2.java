0fcc565c1ddf1fb275d3bcae2c21114ed27ac09d
ike1417 <istudenthernan14@gmail.com>
2025-07-23 05:36:22 +0000

    Done tasksheet-1-1-2 in chapter1

---
+++ Chapter1/Tasksheet_1_1_2.java
@@ -0,0 +1,25 @@
+package Chapter1;
+
+public class Tasksheet_1_1_2 {   
+
+
+    public static void main (String[] args){
+
+        int result = 1 + 2; // result is now 3
+        System.out.println(result); // Output 1
+
+        result = result - 1; // result is now 2
+        System.out.println(result); // Output 2
+
+        result = result * 2; // result is now 4
+        System.out.println(result); // Output 3
+
+        result = result / 2; // result is now 2
+        System.out.println(result); // Output 4
+
+        result = result + 8; // result is now 10
+        result = result % 7; // result is now 3
+        System.out.println(result); // Output 5
+
+    }
+}
